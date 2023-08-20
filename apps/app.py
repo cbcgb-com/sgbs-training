c3658b514398d00b31728d4c7a0f2c9d394240a8
Eric Ma <ericmajinglong@gmail.com>
2023-08-20 12:58:43 -0400

    refactor(app): improve code readability in app.py
    - Reformatted the scripture and create_exercises function calls for better readability.
    - No changes in functionality or API.

--- apps/app.py
+++ apps/app.py
@@ -37,15 +37,21 @@ write_creds()
 
 scripture_choices = [Luke, John, Ephesians, James]
 
-scripture = pn.widgets.Select(name="Scripture", options={s.__name__: s for s in scripture_choices}, )
+scripture = pn.widgets.Select(
+    name="Scripture",
+    options={s.__name__: s for s in scripture_choices},
+)
 
-num_students = pn.widgets.IntSlider(name='Number of Students', start=1, end=30)
-num_groups = pn.widgets.IntSlider(name='Number of Groups', start=1, end=10)
+num_students = pn.widgets.IntSlider(name="Number of Students", start=1, end=30)
+num_groups = pn.widgets.IntSlider(name="Number of Groups", start=1, end=10)
 
 email_html = pn.pane.HTML()
 
+
 def write_email(event):
-    questions, notes = create_exercises(scripture.value, num_students=num_students.value, num_groups=num_groups.value)
+    questions, notes = create_exercises(
+        scripture.value, num_students=num_students.value, num_groups=num_groups.value
+    )
     text = compose_homework_email(scripture.value, questions, notes)
     email_html.object = markdown(text)
 
